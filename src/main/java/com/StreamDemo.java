a834a5bb04f37a381904c17531d2a7ea15c274cb
haribabunasika <haribabu.java4s@gmail.com>
2022-03-12 20:05:17 +0530

    updated from working area local

--- src/main/java/com/StreamDemo.java
+++ src/main/java/com/StreamDemo.java
@@ -4,6 +4,8 @@ public class StreamDemo {
     public static void main(String[] args) {
 
         System.out.println("welcome");
+
+        System.out.println("Updated from working area local");
         
 
     }
