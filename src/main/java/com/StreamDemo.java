780cf85479b450362d1de797e20ef756de30c55a
haribabunasika <101444391+haribabunasika@users.noreply.github.com>
2022-03-12 20:18:04 +0530

    Update StreamDemo.java

--- src/main/java/com/StreamDemo.java
+++ src/main/java/com/StreamDemo.java
@@ -4,10 +4,7 @@ public class StreamDemo {
     public static void main(String[] args) {
 
         System.out.println("welcome");
-
-        System.out.println("Updated from working area local");
-        
-        System.out.println("Newly added from remote repo");
+        System.out.println("Updated from working area local -1");
         
 
     }
