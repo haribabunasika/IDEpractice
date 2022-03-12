6c32b9d07d81d89b2d85271f1450e3338126731a
haribabunasika <haribabu.java4s@gmail.com>
2022-03-12 19:59:47 +0530

    update the streamdemo class

--- src/main/java/com/StreamDemo.java
+++ src/main/java/com/StreamDemo.java
@@ -4,9 +4,7 @@ public class StreamDemo {
     public static void main(String[] args) {
 
         System.out.println("welcome");
-        System.out.println("welcome 2");
-
-        System.out.println("welcome to java");
+        
 
     }
 
