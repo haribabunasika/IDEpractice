ab20426ea79db31d2320081bb1f69ca2f5e0d13f
haribabunasika <101444391+haribabunasika@users.noreply.github.com>
2022-03-12 13:08:32 +0530

    Update StreamDemo.java

--- src/main/java/com/StreamDemo.java
+++ src/main/java/com/StreamDemo.java
@@ -2,6 +2,6 @@ package com;
 
 public class StreamDemo {
     public static void main(String[] args) {
-        System.out.println("welcome");
+        System.out.println("welcome to java");
     }
 }
