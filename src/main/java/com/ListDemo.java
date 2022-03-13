939f78739c54949266133fb16a2463f870568b1d
haribabunasika <101444391+haribabunasika@users.noreply.github.com>
2022-03-13 08:52:09 +0530

    Update ListDemo.java

--- src/main/java/com/ListDemo.java
+++ src/main/java/com/ListDemo.java
@@ -8,6 +8,7 @@ public class ListDemo {
     public static void main(String[] args) {
         List<Integer> list = Arrays.asList(1,2,3,4,6,6,4);
         list.stream().distinct().forEach(System.out::println);
+        System.out.println("Remote changes");
 
 
     }
