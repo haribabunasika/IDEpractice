8b0cf1d21ea00272d0ab0e9a13b012fb33ef6dfb
haribabu-git <haribabu.nasika@gmail.com>
2022-03-13 09:20:37 +0530

    local changes commited

--- src/main/java/com/ListDemo.java
+++ src/main/java/com/ListDemo.java
@@ -8,8 +8,8 @@ public class ListDemo {
     public static void main(String[] args) {
         List<Integer> list = Arrays.asList(1,2,3,4,6,6,4);
         list.stream().distinct().forEach(System.out::println);
-        
-        System.out.println("Remote changes from haribabu.nasika@gmail_dev");
+
+        System.out.println("Remote  and changes from haribabu.nasika@gmail_dev");
 
         System.out.println("Local changes from haribabu.nasika@gmail_dev");
 
